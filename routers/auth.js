c7531f36750ee19cd0b27bd3e94654b46a326113
yamadamasashi <shisama94@gmail.com>
2025-08-11 21:31:40 +0900

    不要なコメント削除

--- routers/auth.js
+++ routers/auth.js
@@ -1,21 +1,19 @@
-const { PrismaClient } = require("@prisma/client");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const generateIdenticon = require("../utils/generateIdenticon");
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const generateIdenticon = require('../utils/generateIdenticon');
 const prisma = new PrismaClient();
 
-// app.use('/〇〇', router) を指定して、それ以降の「中の道順」は router が受け持つ！
-const router = require("express").Router();
+const router = require('express').Router();
 
 // 新規ユーザー登録API
-// postできるかのテストとして、thunder clientというVScodeアプリを使用した。 post先はhttp://localhost:10000
 // /api/auth/register
-router.post("/register", async (req, res) => {
+router.post('/register', async (req, res) => {
   const { username, email, password } = req.body;
 
   const defaultIconImage = generateIdenticon(email);
 
-  const hashPassWord = await bcrypt.hash(password, 10); // hash化できる
+  const hashPassWord = await bcrypt.hash(password, 10);
   const user = await prisma.user.create({
     data: {
       username,
@@ -23,7 +21,7 @@ router.post("/register", async (req, res) => {
       password: hashPassWord,
       profile: {
         create: {
-          bio: "初めまして",
+          bio: '初めまして',
           profileImageUrl: defaultIconImage,
         },
       },
@@ -36,14 +34,14 @@ router.post("/register", async (req, res) => {
 });
 
 // ユーザーログインAPIの作成
-router.post("/login", async (req, res) => {
+router.post('/login', async (req, res) => {
   const { email, password } = req.body;
   // prisma.<model名の小文字>.<メソッド>()
   const user = await prisma.user.findUnique({ where: { email } }); // where条件を絞るという意味
 
   if (!user) {
     return res.status(401).json({
-      error: "メールアドレスかパスワードが間違っています",
+      error: 'メールアドレスかパスワードが間違っています',
     });
   }
 
@@ -51,13 +49,13 @@ router.post("/login", async (req, res) => {
 
   if (!isPasswordVaild) {
     return res.status(401).json({
-      error: "パスワードが間違っています",
+      error: 'パスワードが間違っています',
     });
   }
 
   //tocenの発行をidを用いて行う。＊環境変数は、dotenvライブラリが必要
   const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
-    expiresIn: "1d",
+    expiresIn: '1d',
   }); // expiresIn有効期限
 
   return res.json({ token });
